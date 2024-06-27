05f300cc8e0b2d74a34311d3d8ec23b787e24ed0
Davey <davey.rpt@gmail.com>
2024-06-28 00:21:21 +0800

    update info

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Backend = () => {
     return (
         <div className="skills__content">
-            <h3 className="skills__title">Backend Developer</h3>
+            <h3 className="skills__title">Backend Stack</h3>
 
             <div className="skills__box">
                 <div className="skills__group">
@@ -60,7 +60,7 @@ const Backend = () => {
                     <div className="skills__data">
                         <i className='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Passport</h3>
+                            <h3 className="skills__name">Firebase</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
--- src/components/skills/DevOps.jsx
+++ src/components/skills/DevOps.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Frontend = () => {
+const DevOps = () => {
     return (
         <div className="skills__content">
-            <h3 className="skills__title">Frontend Stack</h3>
+            <h3 className="skills__title">Misc.</h3>
 
             <div className="skills__box">
                 <div className="skills__group">
                     <div className="skills__data">
                         <i className='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">HTML5</h3>
+                            <h3 className="skills__name">AWS Elastic Beanstalk</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
@@ -18,7 +18,7 @@ const Frontend = () => {
                     <div className="skills__data">
                         <i className='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">CSS</h3>
+                            <h3 className="skills__name">Google Firebase</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
@@ -26,48 +26,43 @@ const Frontend = () => {
                     <div className="skills__data">
                         <i className='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Javascript</h3>
+                            <h3 className="skills__name">DigitalOcean Droplet</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
 
-                    <div className="skills__data">
-                        <i className='bx bx-badge-check'></i>
-                        <div>
-                            <h3 className="skills__name">Git</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
                 </div>
-
+ 
                 <div className="skills__group">
                     <div className="skills__data">
                         <i className='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Bootstrap</h3>
+                            <h3 className="skills__name">Docker</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+ 
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Exabyte DNS</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+   
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Nginx</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
 
-                    <div className="skills__data">
-                        <i className='bx bx-badge-check'></i>
-                        <div>
-                            <h3 className="skills__name">Tailwind</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <i className='bx bx-badge-check'></i>
-                        <div>
-                            <h3 className="skills__name">React</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
                 </div>
+
             </div>
         </div>
     )
 }
 
-export default Frontend
+export default DevOps
